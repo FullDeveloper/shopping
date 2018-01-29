93b7486fabc5403faf46b8d7e66fb87f9ecbfe6b
keppelfei <572613610@qq.com>
2018-01-29 15:19:44 +0800

    11

--- src/main/java/com/emed/shopping/dao/mapper/admin/store/ShopUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.emed.shopping.dao.mapper.admin.store;
-
-import com.emed.shopping.dao.model.admin.store.ShopUser;
-import com.emed.shopping.util.CommonMapper;
-
-public interface ShopUserMapper extends CommonMapper<ShopUser>{
-
-}
